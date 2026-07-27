9f117080fb89fa61e394349f7ac51fb87ec6326c
Murage <25152892+0xMurage@users.noreply.github.com>
2026-07-28 00:37:05 +0300

    feat: register middleware

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
-import { Hono } from 'hono'
-import { serveStatic } from 'hono/bun'
+import { Hono } from "hono";
+import { serveStatic } from "hono/bun";
+import { setRenderer } from "./middleware/renderer.ts";
 
-const app = new Hono()
-
-app.use('/static/*', serveStatic({ root: './' }))
+const app = new Hono();
 
 
+app.use(setRenderer());
 
 app.get('/', async(ctx)=>{
 
   return ctx.html("<strong> Hello there. </strong>")
 });
 
-export default app
+export default app;
