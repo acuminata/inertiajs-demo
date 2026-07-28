015671b9ebc02983b3944f96756349d695cd62a6
Murage <25152892+0xMurage@users.noreply.github.com>
2026-07-28 20:14:58 +0300

    feat: watch pages directory for deletion/new files in inertia types generator tool

--- tools/gen-inertia-types.ts
+++ tools/gen-inertia-types.ts
@@ -1,7 +1,14 @@
-import { readdirSync, mkdirSync, writeFileSync } from "node:fs";
+import {
+	readdirSync,
+	mkdirSync,
+	writeFileSync,
+	watch
+} from "node:fs";
 import { resolve, join, relative, dirname } from "node:path";
 import { parseArgs } from "node:util";
 import { cwd, argv } from "node:process";
+
+const DEBOUNCE_INTERVAL = 2000; // Wait time after last change before syncing
 
 const { values } = parseArgs({
 	args: argv.slice(2),
@@ -14,6 +21,19 @@ const { values } = parseArgs({
 
 const PAGES_DIR = resolve(cwd(), values.in || "/src/pages");
 const OUTPUT_FILE = resolve(cwd(), values.out || "/src/inertia.d.ts");
+
+function generateTypes() {
+	const pages = scan(PAGES_DIR).sort();
+	const content = `// Auto-generated file. Any manual changes will be lost when sources are regenerated.
+declare global {
+    type InertiaPage = ${pages.map((p) => `"${p}"`).join(" | ") || "string"};
+}
+export {};
+`;
+
+	mkdirSync(dirname(OUTPUT_FILE), { recursive: true });
+	writeFileSync(OUTPUT_FILE, content);
+}
 
 function scan(dir: string, base = dir): string[] {
 	let pages: string[] = [];
@@ -29,13 +49,24 @@ function scan(dir: string, base = dir): string[] {
 	return pages;
 }
 
-const pages = scan(PAGES_DIR).sort();
-const content = `// Auto-generated file. Any manual changes will be lost when sources are regenerated.
-declare global {
-    type InertiaPage = ${pages.map((p) => `"${p}"`).join(" | ") || "string"};
-}
-export {};
-`;
+let timeoutId: NodeJS.Timeout;
 
-mkdirSync(dirname(OUTPUT_FILE), { recursive: true });
-writeFileSync(OUTPUT_FILE, content);
+// Watch for changes
+watch(PAGES_DIR, { recursive: true }, (eventType) => {
+	if (eventType === "change") return; //ignore when file contents change
+
+	if (timeoutId) {
+		clearTimeout(timeoutId);
+	}
+
+	try {
+		timeoutId = setTimeout(() => {
+			generateTypes();
+		}, DEBOUNCE_INTERVAL);
+	} catch (err: unknown) {
+		// Gracefully catch errors if files are locked
+		console.error("Error occurred while generating types... ", err);
+	}
+});
+
+console.info("Watching 👀for changes...");
